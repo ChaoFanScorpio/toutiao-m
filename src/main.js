775e8f6eb63f48e78f9ada4fabd36159df8971cc
张得丑 <18511141234@163.com>
2022-08-17 17:22:27 +0800

    全局导入Vant

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import router from './router'
 import store from './store'
 // 全局样式
 import './styles/index.less'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router)
+// app.use(Vant)
+app.use(Vant)
+app.mount('#app')
